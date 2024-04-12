cd62567705cb2d06e8dc31f40de3ad246616b503
yaotian wang <1784766951@qq.com>
2024-04-12 15:38:38 +1000

    waiting for report

--- A1.py
+++ A1.py
@@ -6,6 +6,9 @@ from mpi4py import MPI
 from collections import defaultdict
 from collections import Counter
 
+# start the timer
+processing_start_time = datetime.datetime.now()
+
 # mpi related variables
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
@@ -46,8 +49,6 @@ each_bytes = math.ceil(total_bytes / size)
 begin_position = rank * each_bytes
 end_position = (rank + 1) * each_bytes
 
-processing_start_time = datetime.datetime.now()
-
 with open('./twitter-100gb.json', 'r', encoding='utf-8') as twit_file:
 
     twit_file.seek(begin_position)
@@ -78,6 +79,8 @@ processing_time = (processing_end_time - processing_start_time).total_seconds()
 result_list = comm.gather([happy_hour_dict, happy_day_dict, active_hour_dict, active_day_dict,processing_time], root=0)
 
 if rank == 0:
+    output_start_time = datetime.datetime.now()
+
     # collect result
     result_happy_hour_dict = defaultdict(float)
     result_happy_day_dict = defaultdict(float)
@@ -85,7 +88,6 @@ if rank == 0:
     result_active_day_dict = Counter()
 
     # merge result list
-    output_start_time = datetime.datetime.now()
     for dict_list in result_list:
         result_happy_hour_dict = merge(result_happy_hour_dict,dict_list[0])
         result_happy_day_dict = merge(result_happy_day_dict,dict_list[1])
@@ -97,9 +99,15 @@ if rank == 0:
     most_active_hour = max(result_active_hour_dict, key=result_active_hour_dict.get)
     most_active_day = max(result_active_day_dict, key=result_active_day_dict.get)
 
-    print(f"Happiest Hour: {happiest_hour} was the happiest hour with an overall sentiment score of {result_happy_hour_dict[happiest_hour]}")
+    happiest_hour_split = happiest_hour.split('T')
+    most_active_hour_split = most_active_hour.split('T')    
+
+    happiest_hour_hour = int(happiest_hour_split[1])
+    most_active_hour_hour = int(most_active_hour_split[1])
+
+    print(f"Happiest Hour: {happiest_hour_hour}:00-{happiest_hour_hour + 1}:00 on {happiest_hour_split[0]} was the happiest hour with an overall sentiment score of {result_happy_hour_dict[happiest_hour]}")
     print(f"Happiest Day: {happiest_day} was the happiest day with an overall sentiment score of {result_happy_day_dict[happiest_day]}")
-    print(f"Most Active Hour: {most_active_hour} had the most tweets(#{result_active_hour_dict[most_active_hour]})")
+    print(f"Most Active Hour: {most_active_hour_hour}:00-{most_active_hour_hour + 1}:00 on {most_active_hour_split[0]} had the most tweets(#{result_active_hour_dict[most_active_hour]})")
     print(f"Most Active Day: {most_active_day} had the most tweets(#{result_active_day_dict[most_active_day]})")
 
     output_end_time = datetime.datetime.now()
@@ -108,8 +116,10 @@ if rank == 0:
     output_time = (output_end_time - output_start_time).total_seconds()
     processing_times = [item[4] for item in result_list]  
     avg_processing_time = sum(processing_times) / len(processing_times)
+    total_time = (output_end_time - processing_start_time).total_seconds()
 
-    print(f"Average Processing Time: {avg_processing_time} seconds")
-    print(f"Processing Times per Process: {processing_times}")
-    print(f"Communication Time: {communication_time} seconds")
-    print(f"Output Time: {output_time} seconds")
+    print(f"Average Parallel Processing Time: {avg_processing_time} seconds")
+    print(f"Max Parallel Processing Time: {max(processing_times)} seconds")
+    print(f"Parallel Processing Times per Process: {processing_times}")
+    print(f"Output Calculation Time: {output_time} seconds")
+    print(f"Total time: {total_time} seconds")
